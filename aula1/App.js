85ba0af0be00042b9c001deaba9eebb60513d94f
Auridebson Lima <auridebson@vutus.com>
2023-10-15 12:56:46 -0300

    Initial commit

---
+++ aula1/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import Welcome  from './src/pages/Welcome'
+import routes from './src/routes'
+
+// import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Again!!!</Text>
+      <StatusBar style={styles.statusBarContainer} />
+      <Welcome />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ddd',
+    alingItems: 'center',
+    justifyContent: 'center'
+  },
+  statusBarContainer: {
+    backgroundColor: '#000',
+    barStyle: 'light-content'
+  }
+}
+)
---
+++ aula1/src/pages/Welcome/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+export default function Welcome() {
+    return (
+        <View>
+            <Text>Bem vinda a página de Boas Vindas!</Text>
+        </View>
+    )
+}
