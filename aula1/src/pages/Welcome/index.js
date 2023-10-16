138c23aff4f847f9e3793a5541be718197ba6999
Auridebson Lima <auridebson@vutus.com>
2023-10-16 05:44:49 -0300

    Update

--- aula1/src/pages/Welcome/index.js
+++ aula1/src/pages/Welcome/index.js
@@ -1,10 +1,34 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { 
+    View, 
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity
+} from 'react-native';
 
 export default function Welcome() {
     return (
-        <View>
-            <Text>Página de Boas Vindas!</Text>
+        <View style={styles.container}>
+            <View style={styles.containerLogo}>
+            <Image
+                source={require('../../assets/images/img_kravmaga.jpg')}
+                style={{width:'100%'}}
+                resizeMode="contain"
+            />
+            </View>
+
+            <View style={styles.containerForm}>
+                <Text style={styles.title}>Constrole suas atividades e treinos em qualquer lugar</Text>
+                <Text style={styles.text}>Faça o login para começar</Text>
+            </View>
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+})
