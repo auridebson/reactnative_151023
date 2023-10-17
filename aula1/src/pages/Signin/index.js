d24714fd2f3a0d5289aae383d3ed7bbacdecaeb7
Auridebson Lima <auridebson@vutus.com>
2023-10-16 23:41:01 -0300

    Update

--- aula1/src/pages/Signin/index.js
+++ aula1/src/pages/Signin/index.js
@@ -1,10 +1,38 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import * as Animatable from 'react-native-animatable'
 
 export default function Signin() {
     return (
-        <View>
-            <Text>Tela de Login</Text>
+        <View style={styles.container}>
+            <Animatable.View animation="fadeInLeft" delay={600} style={styles.containerHeader}>
+                <Text style={styles.message}>Bem vindo(a)</Text>
+            </Animatable.View>
+
+            <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+                <Text style={styles.title}>Email</Text>
+                <TextInput 
+                    placeholder="Digite um e-mail" 
+                    style={styles.input} 
+                />
+            </Animatable.View>
+            
+            <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+                <Text style={styles.title}>Senha</Text>
+                <TextInput 
+                    placeholder="Sua senha" 
+                    style={styles.input} 
+                />
+            </Animatable.View>
+
+
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+})
