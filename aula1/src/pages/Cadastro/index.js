33cd29aa54bf0c32480da2af7d84f11dd2441e9c
Auridebson Lima <auridebson@vutus.com>
2023-10-17 05:11:23 -0300

    Update

--- aula1/src/pages/Cadastro/index.js
+++ aula1/src/pages/Cadastro/index.js
@@ -5,14 +5,30 @@ import * as Animatable from 'react-native-animatable'
 export default function Cadastro() {
     return (
         <View style={Styles.containerMain}>
+
+            <View style={Styles.containerTopbar}>
+                <Text style={Styles.containerTitle}>Cadastro de Usuário</Text>
+            </View>
             <Text>Cadastro de Usuário</Text>
         </View>
     )
 }
 
-const Styles = {
+const Styles = StyleSheet.create({
     containerMain:{
         flex: 1,
-        flexDirection: 'column'
+        flexDirection: 'column',
+    },
+    containerTopbar:{
+        flexDirection: 'row',
+        height: 70,
+        backgroundColor: '#38a69d'
+    },
+    containerTitle:{
+        color: '#fff',
+        fontSize: 25,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        margin: 20
     }
-}
+})
